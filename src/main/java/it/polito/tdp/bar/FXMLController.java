fd992556cca1622f84cd25c3623fa2552d0104fe
monge <alberto.monge@polito.it>
2022-05-17 17:09:08 +0200

    Progetto base

---
+++ src/main/java/it/polito/tdp/bar/FXMLController.java
@@ -0,0 +1,38 @@
+package it.polito.tdp.bar;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import it.polito.tdp.bar.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class FXMLController {
+
+	@FXML // ResourceBundle that was given to the FXMLLoader
+	private ResourceBundle resources;
+
+	@FXML // URL location of the FXML file that was given to the FXMLLoader
+	private URL location;
+
+	@FXML // fx:id="txtResult"
+	private TextArea txtResult; // Value injected by FXMLLoader
+
+	private Model model;
+
+	@FXML
+	void handleSimula(ActionEvent event) {
+
+	}
+
+	@FXML // This method is called by the FXMLLoader when initialization is complete
+	void initialize() {
+		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+
+	}
+
+	public void setModel(Model model) {
+		this.model = model;
+
+	}
+}
